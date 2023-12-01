8896b9e1694b11de09f759ac53d1deb0b3f8786a
bondpy202212 <bondpy202212@gmail.com>
2023-12-01 18:53:11 +0200

    comment 10853

--- app1.py
+++ app1.py
@@ -1,11 +1,22 @@
+# For PC
+# import os
+
+
+# def run_celery(): 
+#     os.environ['FORKED_BY_MULTIPROCESSING'] = '1'
+#     os.system('celery -A tasks worker --loglevel=info --pool=solo > celery_logs.txt 2>&1')
+
+
+# if __name__ == '__main__':
+#     run_celery()
+
+
+# For VM
 import os
 
-def run_celery(): 
+def run_celery():
     os.environ['FORKED_BY_MULTIPROCESSING'] = '1'
-    os.system('celery -A tasks worker --loglevel=info --pool=solo > celery_logs.txt 2>&1')
-
+    os.system('/home/user_name/newswebsite/env/bin/celery -A your_app_name worker --loglevel=info > celery_logs.txt 2>&1')
 
 if __name__ == '__main__':
-    # with open('celery_logs.txt', 'w') as log_file:
-    #     log_file.write('')
     run_celery()
