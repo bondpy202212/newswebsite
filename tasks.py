a75714d78485857f5f4c52b2b51d49665cfb677e
bondpy202212 <bondpy202212@gmail.com>
2023-12-22 20:37:49 +0200

    comment 25407

--- tasks.py
+++ tasks.py
@@ -54,3 +54,63 @@ if __name__=='__main__':
 # if __name__=='__main__':
 # 	with flask_app.app_context():
 # 		weather_html.weather_by_city()
+
+
+
+
+# from webapp import create_app
+# from webapp.news.parsers import getnews  
+
+# def getnews_snippets():
+# 	with flask_app.app_context():
+# 		getnews.get_news_snippets()
+
+# def getnews_content():
+# 	with flask_app.app_context():
+# 		getnews.get_news_content()
+
+
+# if __name__=='__main__':
+# 	with flask_app.app_context():
+# 		getnews.get_news_snippets()
+# 		getnews.get_news_content()
+
+
+
+# +++++++++++++++++++
+
+from celery import Celery
+from celery.schedules import crontab
+
+
+from webapp import create_app
+from webapp.news.parsers import getnews
+
+
+flask_app = create_app()
+celery_app = Celery('tasks', broker='redis://localhost:6379/0')
+
+
+@celery_app.task
+def getnews_snippets():
+	with flask_app.app_context():
+		getnews.get_news_snippets()
+
+
+@celery_app.task
+def hgetnews_content():
+	with flask_app.app_context():
+		getnews.get_news_content()
+
+
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+	sender.add_periodic_task(crontab(minute='*/1'), getnews_snippets.s())
+	sender.add_periodic_task(crontab(minute='*/1'), getnews_content.s())
+
+
+if __name__ == '__main__':
+    celery_app.conf.update(imports=['app'])
+    celery_app.worker_main(['worker', '--beat'])
+
+# +++++++++++++++++++
