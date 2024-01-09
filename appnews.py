362c2fdc5cef690655b855b177ade2b319e91e86
bondpy202212 <149815146+bondpy202212@users.noreply.github.com>
2024-01-09 17:00:18 +0200

    comment 15151

--- appnews.py
+++ appnews.py
@@ -27,20 +27,30 @@ if __name__ == '__main__':
     # Настройка логирования для записи в файл
     log_file_name = 'app.log'
     logging.basicConfig(filename=log_file_name, level=logging.INFO)
+    workers = 4
+    bind = '0.0.0.0:8000'
+    timeout = 60
+    
+    # -----------            for testing
+    # start = datetime.now()
+    # getnews_snippets()
+    # finish = datetime.now() - start
+    # print(f"time run getnew: {finish}")
+    # getnews_content()
 
-    start = datetime.now()
+    # every(10).seconds.do(getnews_snippets)   
+    # time.sleep(5)
+    # every(10).seconds.do(getnews_content)
+    # -----------            
+
+
     getnews_snippets()
-    finish = datetime.now() - start
-    print(f"time run getnew: {finish}")
+    time.sleep(60)
     getnews_content()
 
-    every(10).seconds.do(getnews_snippets)   
-    time.sleep(5)
-    every(10).seconds.do(getnews_content)
-
-    # every().day.at("12:00").do(getnews_snippets)
-    # time.sleep(10)
-    # every().day.at("12:00").do(getnews_content)
+    every().day.at("12:00").do(getnews_snippets)
+    time.sleep(60)
+    every().day.at("12:00").do(getnews_content)
 
     while True:
         run_pending()
@@ -54,85 +64,36 @@ if __name__ == '__main__':
 # # ++++++++++++++++++++++++++++++++++
 # # ++++++++++++++++++++++++++++++++++
 
-# from schedule import every, run_pending
-# import time
-# import logging
-# from datetime import datetime
+from schedule import every, run_pending
+import time
+import logging
+from datetime import datetime
 
-# from webapp import create_app
-# from webapp.news.parsers import getnews 
-
-
-
-# flask_app = create_app()
-
-# # def add(x, y):
-# #     with flask_app.app_context():
-# #         print('-- hello --')
-# #         print(x + y)
-
-# def getnews_snippets():
-#     with flask_app.app_context():
-#         getnews.get_news_snippets()
-#         logging.info(f'getnews_snippets executed at {datetime.now()}')
-
-# def getnews_content():
-#     with flask_app.app_context():
-#         getnews.get_news_content()
-#         logging.info(f'getnews_content executed at {datetime.now()}')
-
-# # @app.route('/')
-# # def index():
-# #    return 'Hello, World!'
+from webapp import create_app
+from webapp.news.parsers import getnews 
 
 
-# if __name__ == '__main__':
-#     logging.basicConfig(level=logging.INFO)
-#     logger = logging.getLogger(__name__)
+flask_app = create_app()
+
+def getnews_snippets():
+    with flask_app.app_context():
+        getnews.get_news_snippets()
+        logging.info(f'getnews_snippets: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
 
-#     # Запускать задачу каждые 10 секунд
-#     # every(1).seconds.do(add, 2, 3)
-#     every(10).seconds.do(getnews_snippets)
-#     every(10).seconds.do(getnews_content)
-#     # -----------------------------
-#     # every().day.at("12:00").do(add, 2, 3)
+def getnews_content():
+    with flask_app.app_context():
+        getnews.get_news_content()
+        logging.info(f'getnews_content : {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
-#     while True:
-#         run_pending()
-#         time.sleep(1)
+if __name__ == '__main__':
+    log_file_name = 'app.log'
+    logging.basicConfig(filename=log_file_name, level=logging.INFO)
 
+    getnews_snippets()
+    time.sleep(60)
+    getnews_content()
 
-
-
-
-
-
-
-# # ++++++++++++++++++++++++++++++++++
-# # ++++++++++++++++++++++++++++++++++
-# from schedule import every, run_pending
-# import time
-
-# from webapp import create_app
-# from webapp.news.parsers import getnews 
-
-
-# flask_app = create_app()
-
-# def getnews_snippets():
-#     with flask_app.app_context():
-#         getnews.get_news_snippets()
-
-# def getnews_content():
-#     with flask_app.app_context():
-#         getnews.get_news_content()
-
-
-# if __name__ == '__main__':
-#     # Запускать задачу каждые 10 секунд
-#     every(10).seconds.do(add, 2, 3)
-
-#     while True:
-#         run_pending()
-#         time.sleep(1)
+    while True:
+        run_pending()
+        time.sleep(1)
