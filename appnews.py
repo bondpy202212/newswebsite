992effaa2258031a3eff494ef7454f1cb048fd36
bondpy202212 <149815146+bondpy202212@users.noreply.github.com>
2024-01-10 11:52:50 +0200

    comment 7678

--- appnews.py
+++ appnews.py
@@ -1,40 +1,92 @@
-from schedule import every
-import time
-import threading
 import logging
 from datetime import datetime
+
+from celery import Celery
+from celery.schedules import crontab
 
 from webapp import create_app
 from webapp.news.parsers import getnews
 
-# Создание приложения Flask
-# flask_app = create_app()
-app = create_app()
-flask_app = app
+flask_app = create_app()
+celery_app = Celery('tasks', broker='redis://localhost:6379/0')
 
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger(__name__)
+# Настройка логирования для записи в файл
+log_file_name = 'app.log'
+logging.basicConfig(filename=log_file_name, level=logging.INFO)
 
+@celery_app.task
 def getnews_snippets():
     with flask_app.app_context():
         getnews.get_news_snippets()
-        log.info(f'getnews_snippets: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        logging.info(f'getnews_snippets: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
+@celery_app.task
 def getnews_content():
     with flask_app.app_context():
         getnews.get_news_content()
-        log.info(f'getnews_content : {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        logging.info(f'getnews_content : {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
-def schedule_jobs():
-    while True:
-        every(10).seconds.do(getnews_snippets)
-        every(10).seconds.do(getnews_content)
-        time.sleep(10)  # Пауза перед выполнением следующей планировки
+@celery_app.task
+def test_function():
+    with flask_app.app_context():        
+        logging.info(f'getnews_content : {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
-if __name__ == '__main__':
-    t = threading.Thread(target=schedule_jobs)
-    t.start()
-    flask_app.run()
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # sender.add_periodic_task(crontab(second='*/10'), getnews_snippets.s())
+    # sender.add_periodic_task(crontab(second='*/10'), getnews_content.s())
+    sender.add_periodic_task(crontab(second='*/10'), test_function.s())
+
+if __name__ == "__main__":
+    celery_app.start()
+
+
+# for start gunicorn from terminal: gunicorn main:app --bind localhost:8000 --workers 4 --timeout 120 --log-level info
+
+
+
+
+
+# # # ----------- 
+# # # with schedule (not working in VM, procces schedule not started in supervisor)
+# # # -----------  
+# from schedule import every
+# import time
+# import threading
+# import logging
+# from datetime import datetime
+
+# from webapp import create_app
+# from webapp.news.parsers import getnews
+
+# # Создание приложения Flask
+# # flask_app = create_app()
+# app = create_app()
+# flask_app = app
+
+# logging.basicConfig(level=logging.INFO)
+# log = logging.getLogger(__name__)
+
+# def getnews_snippets():
+#     with flask_app.app_context():
+#         getnews.get_news_snippets()
+#         log.info(f'getnews_snippets: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+
+# def getnews_content():
+#     with flask_app.app_context():
+#         getnews.get_news_content()
+#         log.info(f'getnews_content : {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+
+# def schedule_jobs():
+#     while True:
+#         every(10).seconds.do(getnews_snippets)
+#         every(10).seconds.do(getnews_content)
+#         time.sleep(10)  # Пауза перед выполнением следующей планировки
+
+# if __name__ == '__main__':
+#     t = threading.Thread(target=schedule_jobs)
+#     t.start()
+#     flask_app.run()
 
 
 
@@ -142,8 +194,8 @@ if __name__ == '__main__':
 # log_file_name = 'app.log'
 # logging.basicConfig(filename=log_file_name, level=logging.INFO)
 
-# getnews_snippets()
-# getnews_content()
+# every(10).seconds.do(getnews_snippets) 
+# every(10).seconds.do(getnews_content)
 
 # def app():
 #     while True:
@@ -154,6 +206,8 @@ if __name__ == '__main__':
 #     app()
 
 
+# getnews_snippets()
+# getnews_content()
 
 
 
