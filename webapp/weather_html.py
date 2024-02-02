b507f70f78e92022c39e61a4a66d5d249c327d0a
bondpy202212 <149815146+bondpy202212@users.noreply.github.com>
2024-02-02 18:32:57 +0200

    comment 21808

--- webapp/weather_html.py
+++ webapp/weather_html.py
@@ -10,6 +10,7 @@ def weather_by_city(city_name):
 		'lat': current_app.config['LAT_CITY'],
 		'lon': current_app.config['LON_CITY'],
 		'units': 'metric',
+		# 'units': 'imperial',
 		'appid': current_app.config['WEATHER_API_KEY']				
 	}
 	
@@ -18,6 +19,13 @@ def weather_by_city(city_name):
 		result = requests.get(weather_url, params=params1)
 		result.raise_for_status()                 # status answer server (error 4xx, 5xx)
 		weather = result.json()
+
+		print(weather)
+		print('temp_C    :' ,round(weather["main"]["temp"], 1))
+		print('pressure  :' ,round(weather["main"]["pressure"], 1))
+		print('humidity  -:' ,round(weather["main"]["humidity"], 1))
+		print('visibility:' ,round((weather["visibility"])/1000, 1))
+		print('wind_speed:' ,round((weather['wind']["speed"]), 1))
 
 		if "main" in weather:
 			if "temp" in weather["main"]:
@@ -31,7 +39,15 @@ def weather_by_city(city_name):
 									'humidity'	: round(weather["main"]["humidity"], 1),
 									'visibility': round((weather["visibility"])/1000, 1),
 									'wind_speed': round((weather['wind']["speed"]), 1),
-									'wind_deg'	: degree_result(weather['wind']["deg"])									
+									'wind_deg'	: degree_result(weather['wind']["deg"]),
+
+									'temp_F'	: round((weather["main"]["temp"]*1.8)+32, 1),
+									'FeelsLikeF': round((weather["main"]["feels_like"]*1.8)+32, 1),
+									'temp_minF'	: round((weather["main"]["temp_min"]*1.8)+32, 1),
+									'temp_maxF'	: round((weather["main"]["temp_max"]*1.8)+32, 1),
+									'pressure_inHg'	: round(weather["main"]["pressure"]*0.029529983071445, 1),
+									'visibility_mi': round((weather["visibility"])/1000*0.621371, 1),
+									'wind_speed_mph': round((weather['wind']["speed"])*2.23693629, 1),
 									}
 					return current_condition
 				except(IndexError, TypeError): # (server -> give wrong data)
